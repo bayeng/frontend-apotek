929fb6d9d780496dc8262ce9258b9e897be9b12e
khoican <88487817+khoican@users.noreply.github.com>
2024-06-11 21:02:28 +0700

    feat: add link to navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,25 +1,13 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary sticky-top shadow-sm">
     <div class="container-fluid px-5 d-flex justify-content-between">
-        <a class="navbar-brand" href="#">APOTEK RSU</a>
+        <a class="navbar-brand fw-semibold" href="#">APOTEK RSU</a>
         <ul class="navbar-nav">
             <!-- Avatar -->
             <li class="nav-item dropdown">
-                <a
-                    data-mdb-dropdown-init
-                    class="nav-link dropdown-toggle d-flex align-items-center"
-                    href="#"
-                    id="navbarDropdownMenuLink"
-                    data-bs-toggle="dropdown"
-                    role="button"
-                    aria-expanded="false"
-                >
-                    <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (31).webp"
-                        class="rounded-circle"
-                        height="22"
-                        alt="Portrait of a Woman"
-                        loading="lazy"
-                    />
+                <a data-mdb-dropdown-init class="nav-link dropdown-toggle d-flex align-items-center" href="#"
+                    id="navbarDropdownMenuLink" data-bs-toggle="dropdown" role="button" aria-expanded="false">
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (31).webp" class="rounded-circle"
+                        height="22" alt="Portrait of a Woman" loading="lazy" />
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
                     <li>
--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,3 +1,7 @@
-<nav class="position-fixed bg-primary vh-100" style="width: 15%">
-    <a href="" class="text-white p-3 w-100">Home</a>
+<nav class="position-fixed bg-primary vh-100 p-2" style="width: 15%">
+    <a href="/" class="text-white p-2 w-100 nav-link sidebar-link"><i class="bi bi-columns-gap"></i> Dashboard</a>
+    <a href="/supliers" class="text-white p-2 w-100 nav-link sidebar-link"><i class="bi bi-inboxes-fill"></i>
+        Suplier</a>
+    <a href="/obats" class="text-white p-2 w-100 nav-link sidebar-link"><i class="bi bi-capsule-pill"></i> Obat</a>
+    <a href="/users" class="text-white p-2 w-100 nav-link sidebar-link"><i class="bi bi-person-fill"></i> Pengguna</a>
 </nav>
