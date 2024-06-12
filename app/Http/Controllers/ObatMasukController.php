387db4987d5937488aef59e231e34c4e9fe631db
bayeng <88835193+bayeng@users.noreply.github.com>
2024-06-12 12:00:22 +0700

    Update ObatMasukController.php

--- app/Http/Controllers/ObatMasukController.php
+++ app/Http/Controllers/ObatMasukController.php
@@ -3,8 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ObatMasukController extends Controller
 {
-    //
+    protected $apiUrl;
+
+    public function __construct()
+    {
+        $this->apiUrl = config('services.api.url');
+    }
+    public function index()
+    {
+        $url = "{$this->apiUrl}/obatmasuks";
+        $response = Http::get($url);
+        if ($response->successful()) {
+            $data = $response->json();
+            $obatMasuks = $data['data'] ?? [];
+        } else {
+            $obatMasuks = [];
+
+        }
+
+        // Data Relations
+        $supliers = Http::get("{$this->apiUrl}/supliers")->json();
+        $users = Http::get("{$this->apiUrl}/users")->json();
+        $obats = Http::get("{$this->apiUrl}/obats")->json();
+
+        return view('pages.obatmasuks.index', [
+            'obatMasuks'=>$obatMasuks,
+            'users'=>$users['data'],
+            'obats'=>$obats['data'],
+            'supliers'=>$supliers['data']
+        ]);
+    }
+
+    public function storePage()
+    {
+        return view('pages.obatmasuks.add');
+    }
+
+    public function store(Request $request)
+    {
+
+    }
 }
