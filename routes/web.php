d15c5180d01bfc6ae80908b6b42ffeeff843caad
bayeng <88835193+bayeng@users.noreply.github.com>
2024-06-12 08:20:47 +0700

    add: obatmasuks in routes

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,11 @@ Route::prefix('users')->group(function () {
 
 });
 
+Route::prefix('obatmasuks')->group(function (){
+    Route::get('/', [\App\Http\Controllers\ObatMasukController::class, 'index']);
+    Route::get('/{id}', [\App\Http\Controllers\ObatMasukController::class, 'show'])->name('detail-obatmasuk');
+    Route::post('/', [\App\Http\Controllers\ObatMasukController::class, 'store'])->name('store-obatmasuk');
+    Route::get('/add', [\App\Http\Controllers\ObatMasukController::class, 'storePage'])->name('storepage-obatmasuk');
+});
+
 
